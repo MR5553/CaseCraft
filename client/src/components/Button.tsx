eabe8d0c86317796a0607203376085eece848c25
MR5553 <shauryakumar42040er@gmail.com>
2025-08-24 03:30:18 +0530

    feat(component): add Button component with support for icons and variants

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { cn } from "../lib/utils";
+import { type VariantProps } from "class-variance-authority";
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonVariants } from "../lib/ButtonVariant";
+
+
+interface btn extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof ButtonVariants> {
+    children: ReactNode;
+    startIcon?: ReactNode;
+    endIcon?: ReactNode;
+}
+
+export const Button = ({ children, className, variant, size, endIcon, startIcon, ...props }: btn) => {
+    return (
+        <button
+            className={cn(ButtonVariants({ variant, size, className }))}
+            {...props}
+        >
+            {startIcon && startIcon}
+            {children}
+            {endIcon && endIcon}
+        </button>
+    )
+};
