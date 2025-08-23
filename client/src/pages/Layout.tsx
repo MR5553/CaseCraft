ef21494a05c0acafb4aa73318a4b69a27a9bc1af
MR5553 <shauryakumar42040er@gmail.com>
2025-08-24 03:33:38 +0530

    feat(pages): add PIRVATE and PUBLIC components for route protection and redirection

---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,28 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../store/auth.store"
+
+export function PIRVATE() {
+    const { user, isAuthenticated } = useAuth();
+    const location = useLocation();
+
+    if (!isAuthenticated) {
+        return <Navigate to="sign-in" state={{ from: location }} replace />
+    }
+
+    if (user && !user.isVerified) {
+        return <Navigate to={`/verify-otp/${user._id}`} state={{ from: location }} replace />
+    }
+
+    return <Outlet />
+}
+
+export function PUBLIC() {
+    const { user, isAuthenticated } = useAuth();
+    const location = useLocation();
+
+    if (isAuthenticated && user.isVerified) {
+        return <Navigate to="/" state={{ from: location }} replace />
+    }
+
+    return <Outlet />
+}
