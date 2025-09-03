f742c28dee7e254aa8a03dd1f67e9dbb070ca6f2
MR5553 <shauryakumar42040er@gmail.com>
2025-09-04 01:33:26 +0530

    update: package name and global error handler

--- server/src/utils/globalErrorHandler.ts
+++ server/src/utils/globalErrorHandler.ts
@@ -1,15 +1,19 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
-import { apiError } from "../utils/apiError";
+
+interface Err extends Error {
+    status?: number;
+    errors?: string[];
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const globalErrorHandler: ErrorRequestHandler = (err: apiError, _req: Request, res: Response, _next: NextFunction) => {
+export const globalErrorHandler: ErrorRequestHandler = (err: Err, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || 500;
     const message = err.message || "Internal Server Error";
 
     res.status(status).json({
         status,
-        success: false,
         message,
+        success: false,
         errors: err.errors || [],
         stack: err.stack
     });
